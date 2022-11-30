cb1ce875ed6e37edb1bc362164c040ca696b0ef7
Britt Cyr <cyrbritt@gmail.com>
2022-11-30 13:53:12 -0500

    Only allow dao wallet

--- programs/staking-options/src/instructions/exercise.rs
+++ programs/staking-options/src/instructions/exercise.rs
@@ -142,7 +142,7 @@ impl<'info> Exercise<'info> {
         // Verify that it is owned by DUAL.
         assert_keys_eq!(
             self.fee_quote_account.owner.key().to_string(),
-            "CZqTD3b3oQw8cDK4CBddpKF6epA1fR36GBbvU5VBt2Dz"
+            "7Z36Efbt7a4nLiV7s5bY7J2e4TJ6V9JEKGccsy2od2bE"
         );
 
         // Verify expiration
