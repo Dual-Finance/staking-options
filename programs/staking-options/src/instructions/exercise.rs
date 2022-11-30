d8a80aacf2dd989939a3371ac87d0e031cefbc9e
Britt Cyr <cyrbritt@gmail.com>
2022-11-30 13:48:39 -0500

    exercise allow dao and not just squads

--- programs/staking-options/src/instructions/exercise.rs
+++ programs/staking-options/src/instructions/exercise.rs
@@ -140,9 +140,11 @@ impl<'info> Exercise<'info> {
         );
 
         // Verify that it is owned by DUAL.
-        assert_eq!(
-            self.fee_quote_account.owner.key().to_string(),
-            "CZqTD3b3oQw8cDK4CBddpKF6epA1fR36GBbvU5VBt2Dz"
+        assert!(
+            self.fee_quote_account.owner.key().to_string()
+                == "CZqTD3b3oQw8cDK4CBddpKF6epA1fR36GBbvU5VBt2Dz"
+                || self.fee_quote_account.owner.key().to_string()
+                    == "7Z36Efbt7a4nLiV7s5bY7J2e4TJ6V9JEKGccsy2od2bE"
         );
 
         // Verify expiration
